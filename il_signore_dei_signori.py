bc5abef50c60a2b28f8eee546d739b581563d24e
Alecap91 <78102118+Alecap91@users.noreply.github.com>
2021-01-27 19:34:13 +0100

    Add files via upload

--- il_signore_dei_signori.py
+++ il_signore_dei_signori.py
@@ -2,7 +2,7 @@ risposte_giuste = 0
 risposta_1 = str(input("\nCompleta la frase: \n'Questa amico mio non è una ...'"))
 
 
-if str(risposta_1) == 'Miniera' or 'una miniera' or 'Moria':
+if str(risposta_1) == ('Miniera' or 'una miniera' or 'Moria'):
     print('\n\nBravo')
     risposte_giuste+=1
     print('\nLe tue risposte giuste:', risposte_giuste)
@@ -12,7 +12,7 @@ else:
     print('\nLe tue risposte giuste:', risposte_giuste)
 
 risposta_2 = input("\nRispondi alla domanda: \nCosa vuoi Gandalf il Rimbambito? ")
-if risposta_2 == 'La chiave di Orthanc' or 'La chiave di Ortanc' or 'La chiave di Ortnac':
+if risposta_2 == ('La chiave di Orthanc' or 'La chiave di Ortanc' or 'La chiave di Ortnac'):
     print('\n\nBravo')
     risposte_giuste+=1
     print('\nLe tue risposte giuste:', risposte_giuste)
@@ -22,7 +22,7 @@ else:
     print('\nLe tue risposte giuste:', risposte_giuste)
 
 risposta_3 = input("\nCompleta la frase: \nProvi lei a telefonare a Richard Benson e provi a urlare come urlo io ")
-if risposta_3 == 'POLLO' or 'UN POLLO!' or 'POLLO!' or 'Un pollo!':
+if risposta_3 == ('POLLO' or 'UN POLLO!' or 'POLLO!' or 'Un pollo!'):
     print('\nBravo')
     risposte_giuste+=1
     print('\nLe tue risposte giuste:', risposte_giuste)
